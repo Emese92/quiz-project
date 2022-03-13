a59433aae85b167e6638797a9ba5754084513e2e
Emese92 <gregor.emese@gmail.com>
2022-03-13 22:52:17 +0000

    Import libraries and add main function for the game

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import pyfiglet
+
+
+
+def add_name():
+    """
+    Asks for player name and expain the game.
+    """
+    name = input('To start please enter your name: ')
+    print(f"Welcome {name}!\n")
+    print("Can you solve 10 random math problems without a calculator?\n")
+    print("You will have 5 seconds for each problems.\n")
+    print(f"Good luck!\n")
+
+    
+
+
+def main():
+    """
+    Run all program functions
+    """
+    add_name()
+
+print(pyfiglet.figlet_format("Math Quiz", justify="center"))
+main()
