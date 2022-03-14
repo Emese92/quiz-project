2b7b16e0215c2cd0d6c3a00b8750fea0912d8428
Emese92 <gregor.emese@gmail.com>
2022-03-14 18:45:40 +0000

    Add game function with random questions and score count

--- run.py
+++ run.py
@@ -2,17 +2,18 @@ import random
 import pyfiglet
 
 menu_options = {
-    's': 'Start',
-    'r': 'Restart',
-    'e': 'Exit',
+    "s": "Start",
+    "r": "Restart",
+    "e": "Exit",
 }
+
 
 def game_menu():
     """
     Displays menu options for the player to choose from.
     """
     for key in menu_options.keys():
-        print('Press -', key, '- to -', menu_options[key])
+        print("Press -", key, "- to -", menu_options[key])
     while True:
         print("\n")
         game_choice = input("What would you like to do? \n")
@@ -20,7 +21,6 @@ def game_menu():
         if game_choice == "s":
             break
         elif game_choice == "r":
-            restart_game()
             print("Restarting...")
         elif game_choice == "e":
             quit()
@@ -41,7 +41,35 @@ def add_name():
     print("You will have 5 seconds for each problems.\n")
     print(f"Good luck!\n")
 
-  
+def game():
+    """
+    The actual game function.
+    Provides 10 random math calculations.
+    When correct answer is given it adds one to the score.
+    """
+    questions = {}
+    score = 0
+    for i in range(10):
+        int_x = random.randint(0,10)
+        int_y = random.randint(0,10)
+        operators = ["+", "-", "*"]
+        operator_value = random.choice(operators)
+        question = str(int_x)+" "+ str(operator_value)+ " "+str(int_y)
+        answer = eval(question)
+        question+=" = "
+        
+        questions.update({question:str(answer)})
+
+
+    for q in questions.keys():
+        user_answer = input(q)
+        if questions.get(q) == str(user_answer):
+            score += 1
+            print("Correct!")
+        else:
+            print(f"Incorrect! The answer was {answer}")
+    print(f"You got {score}/10")
+    game_menu()
 
 
 def main():
@@ -50,6 +78,7 @@ def main():
     """
     game_menu()
     add_name()
+    game()
 
 print(pyfiglet.figlet_format("Math Quiz", justify="center"))
 main()
