0c5b2a42e34949aaaa66bd3dc29e331999df3346
Emese92 <gregor.emese@gmail.com>
2022-03-16 14:04:40 +0000

    Attach spreadsheet to upload score

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
 import random
 import pyfiglet
 from termcolor import cprint
+import gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Math_quiz_scoreboard')
 
 
 menu_options = {
